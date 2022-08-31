ab0a47e49ead9e0dd82f88607e0932b4282d2091
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-09-01 00:02:16 +0300

    добавляет решение задания

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listToUpdate = document.querySelector("#ingredients");
+
+const elements = ingredients.map(ingredient => {
+  const listItemEl = document.createElement("li");
+  listItemEl.textContent = ingredient;
+  listItemEl.classList.add("item");
+  return listItemEl;
+});
+
+listToUpdate.append(...elements);
+
+
+
+
+
