f743000a96632afb4e6fb63a3dc5b60f8f88c665
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-09-01 13:07:21 +0300

    меняет цикл for of на forEach

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -3,11 +3,8 @@ console.log(`Number of categories: ${categories.length}`);
 
 
 const headings = document.querySelectorAll("h2");
-for (const heading of headings) {
-    console.log(`Category: ${heading.textContent}`);
-
+headings.forEach(heading => {
+    console.log(`Category: ${heading.textContent}`)
     const list = heading.nextElementSibling;
-    console.log(`Elements: ${list.children.length}`);
-    
-    }
-    
+    console.log(`Elements: ${list.children.length}`)
+});
