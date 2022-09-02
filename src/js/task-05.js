2e048c08883aad12691bc735cda0faba8ef819a7
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-09-02 21:44:49 +0300

    добавляет решение задания

---
+++ src/js/task-05.js
@@ -0,0 +1,10 @@
+const refs = {
+    nameInput: document.querySelector('#name-input'),
+    nameOutput: document.querySelector('#name-output'),
+};
+
+refs.nameInput.addEventListener('input', onInputEnteredOutputNameChange);
+
+function onInputEnteredOutputNameChange(event) {
+    refs.nameOutput.textContent = event.currentTarget.value;
+};
