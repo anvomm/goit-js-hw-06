097932ce112b6406b0cd166ab8914450625d9390
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-09-02 15:39:33 +0300

    добавляет решение задания

---
+++ src/js/task-04.js
@@ -0,0 +1,21 @@
+let counterValue = 0;
+
+const refs = {
+    decrementButton: document.querySelector('[data-action="decrement"]'),
+    incrementButton: document.querySelector('[data-action="increment"]'),
+    counterValue: document.querySelector('#value'),
+
+};
+
+refs.decrementButton.addEventListener('click', onDecrementButtonClick);
+refs.incrementButton.addEventListener('click', onIncrementButtonClick);
+
+function onDecrementButtonClick() {
+    counterValue -= 1;
+    refs.counterValue.textContent = counterValue;
+};
+
+function onIncrementButtonClick() {
+    counterValue += 1;
+    refs.counterValue.textContent = counterValue;
+};
