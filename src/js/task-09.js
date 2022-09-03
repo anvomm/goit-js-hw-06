c343cc62d569f9287d0a7c5f59595c5658898051
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-09-03 23:47:49 +0300

    добавляет решение задания

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  button: document.querySelector('button.change-color'),
+  body: document.querySelector('body'),
+  colorName: document.querySelector('span.color'),
+}
+
+refs.button.addEventListener('click', onClickEvent);
+
+function onClickEvent() {
+  refs.colorName.textContent = getRandomHexColor();
+  refs.body.style.backgroundColor = refs.colorName.textContent;
+}
+
