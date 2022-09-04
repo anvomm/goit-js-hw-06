9f7dfeebd287ec36f70ed5b54bb4a133706f02ae
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-09-04 16:43:21 +0300

    добавляет решение задания

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,43 @@
+const refs = {
+  createButton: document.querySelector('button[data-create]'),
+  destroyButton: document.querySelector('button[data-destroy]'),
+  input: document.querySelector('input'),
+  boxesContainer: document.querySelector('#boxes'),
+};
+
+refs.input.addEventListener('input', onInputEvent);
+refs.createButton.addEventListener('click', onCreateButtonClickEvent);
+refs.destroyButton.addEventListener('click', onDestroyButtonClickEvent);
+
+let count = 0;
+
+function onInputEvent(event) {
+  count = event.target.value;
+  return count;  
+}
+
+function onCreateButtonClickEvent() {
+  createBoxes(count);
+};
+
+function onDestroyButtonClickEvent() {
+  const boxes = document.querySelectorAll('.box');
+  boxes.forEach(box => box.remove());
+}
+
+function createBoxes(amount) {
+  const markup = '<div class = box></div>'.repeat(amount);
+  refs.boxesContainer.insertAdjacentHTML('beforeend', markup); 
+  const boxes = document.querySelectorAll('.box');
+
+  boxes.forEach((element, index) => {
+    element.style.width = `${30 + 10 * index}px`;
+    element.style.height = `${30 + 10 * index}px`;
+    element.style.backgroundColor = getRandomHexColor();
+  })
+
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
